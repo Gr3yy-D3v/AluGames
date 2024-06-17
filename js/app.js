2134e9a1ce6bbc80a6b82bac7fce7275dd91d9ec
Raph43lD3V <raphael-lacerda75@hotmail.com>
2024-06-17 16:51:45 -0300

    jogo pronto

--- js/app.js
+++ js/app.js
@@ -1,8 +1,25 @@
-let soldOut = document.getElementById(`game-1`);
-function alterarStatus(){
-    if(soldOut.classList.contains("dashboard__item__img")){
-        soldOut.classList.toggle('dashboard__item__img--rented');
-        console.log('Você me apertou!');
-    } 
+function alterarStatus(gameId) {
+    // Seleciona o elemento do jogo e o botão baseado no ID passado
+    let gameElement = document.getElementById(`game-${gameId}`);
+    let buttonElement = document.getElementById(`button-${gameId}`);
+    let gameNameElement = document.getElementById(`name-${gameId}`);
+    let gameName = gameNameElement.innerText;
+
+    // Alterna a classe de disponibilidade do jogo
+    if (gameElement.classList.contains('dashboard__item__img--rented')) {
+        gameElement.classList.remove('dashboard__item__img--rented');
+    } else {
+        gameElement.classList.add('dashboard__item__img--rented');
+    }
+
+    // Alterna a classe e o texto do botão
+    if (buttonElement.classList.contains('dashboard__item__button--return')) {
+        buttonElement.classList.remove('dashboard__item__button--return');
+        buttonElement.innerHTML = 'Alugar';
+        alert(`Muito obrigado por devolver o jogo, ${gameName}!`)
+    } else {
+        buttonElement.classList.add('dashboard__item__button--return');
+        buttonElement.innerHTML = 'Devolver';
+        alert(`Parabéns, você alugou ${gameName}!`)
+    }
 }
-soldOut.addEventListener('click', alterarClasse);
